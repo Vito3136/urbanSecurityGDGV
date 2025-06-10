94c22522fe677de200f2f396e66c088556c1147b
Vito3136 <105723020+Vito3136@users.noreply.github.com>
2025-06-10 12:42:10 +0200

    - edited 7_BlockPosKernel.py

---
+++ src3/7_BlockPosKernel.py
@@ -0,0 +1,75 @@
+import time, gc
+
+from src3.SVMUtils import executeSVM
+from src3.bytecode_manager import *
+from src3.bytekernels import *
+from joblib import Parallel, delayed
+from pathlib import Path
+from datetime import datetime
+
+NUM_CORES = 8
+
+current_time = datetime.now().strftime("%H:%M:%S")
+print("Current time: " + current_time)
+
+base_dir = Path(__file__).parent
+goodwares_path = (base_dir.parent / "resources" / "goodware_dataset").resolve()
+non_valid_goodwares_path = (base_dir.parent / "resources" / "non_valid_goodwares").resolve()
+malwares_path = (base_dir.parent / "resources" / "malware_dataset").resolve()
+non_valid_malwares_path = (base_dir.parent / "resources" / "non_valid_malwares").resolve()
+
+
+# Creazione delle liste di bytecodes binari
+goodware_bytecodes = collect_bytecodes(str(goodwares_path), str(non_valid_goodwares_path))
+malware_bytecodes = collect_bytecodes(str(malwares_path), str(non_valid_malwares_path))
+
+for i in range(3, 51):
+    for j in range(2, 51):
+        if (i > j):
+            start = time.time()
+            blockPosKernel = BlockPosKernel(block_size=i, pos=j)
+
+            def filter(b):
+                result = blockPosKernel(b)
+                gc.collect()
+                return result
+
+            # Filtraggio dei bytecodes
+            goodware_bytecodes_filtered_with_Block_Pos_Kernel = []
+            goodware_bytecodes_filtered_with_Block_Pos_Kernel = Parallel(n_jobs=NUM_CORES)(
+                delayed(filter)(b) for b in goodware_bytecodes
+            )
+
+            # Filtraggio dei bytecodes
+            malware_bytecodes_filtered_with_Block_Pos_Kernel = []
+            malware_bytecodes_filtered_with_Block_Pos_Kernel = Parallel(n_jobs=NUM_CORES)(
+                delayed(filter)(b) for b in malware_bytecodes
+            )
+
+            # Calcolata la lunghezza maggio tra goodwares e malwares
+            lenBiggestGoodware = get_dimension_biggest_bytecode(goodware_bytecodes_filtered_with_Block_Pos_Kernel)
+            lenBiggestMalware = get_dimension_biggest_bytecode(malware_bytecodes_filtered_with_Block_Pos_Kernel)
+
+            # Si effettua lo zero-padding creando array di lunghezza del piu grande tra malware e goodware + 8
+            if (lenBiggestGoodware > lenBiggestMalware):
+                lenDef = lenBiggestGoodware + 8
+                goodware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding = Parallel(n_jobs=NUM_CORES)(delayed(pad_bytecode)(b, lenDef) for b in goodware_bytecodes_filtered_with_Block_Pos_Kernel)
+                malware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding = Parallel(n_jobs=NUM_CORES)(delayed(pad_bytecode)(b, lenDef) for b in malware_bytecodes_filtered_with_Block_Pos_Kernel)
+            else:
+                lenDef = lenBiggestMalware + 8
+                goodware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding = Parallel(n_jobs=NUM_CORES)(delayed(pad_bytecode)(b, lenDef) for b in goodware_bytecodes_filtered_with_Block_Pos_Kernel)
+                malware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding = Parallel(n_jobs=NUM_CORES)(delayed(pad_bytecode)(b, lenDef) for b in malware_bytecodes_filtered_with_Block_Pos_Kernel)
+
+            # Pulizia
+            goodware_bytecodes_filtered_with_Block_Pos_Kernel.clear()
+            malware_bytecodes_filtered_with_Block_Pos_Kernel.clear()
+
+            print("Filtered with filter Block Pos Kernel with parameters block_size = " + str(i) + " pos = " + str(j))
+            executeSVM(goodware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding, malware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding)
+
+            goodware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding.clear()
+            malware_bytecodes_filtered_with_Block_Pos_Kernel_zero_padding.clear()
+            gc.collect()
+            end = time.time()
+
+            print("Time taken: {:.2f} minutes".format((end - start) / 60))
